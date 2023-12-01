cdccb9faa3c8b559fbdb3beb055f4ca1ac61ba53
jus9298 <58887561+jus9298@users.noreply.github.com>
2023-12-01 14:31:03 +0900

    .

--- project_daum_movie/main.py
+++ project_daum_movie/main.py
@@ -1,6 +1,7 @@
 
 
 from collect.collect_daum_movie_review import review_collector
+from db.movie_dao import get_last_review
 
 
 def main():
@@ -9,7 +10,17 @@ def main():
     print("="*100)
     movie_code = input("영화 코드>> ") #169328
     print("MSG: 매일 12시간에 수집")
-    review_collector(movie_code)
+
+    last_date = get_last_review()
+    # print(last_date)
+    # exit()
+
+    if last_date == None:
+        last_date = 0
+    else:
+        last_date = (last_date["int_regdate"])
+c
+    review_collector(movie_code, last_date)
 
 
 if __name__ == "__main__":
